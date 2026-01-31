34417cc5e72c731f177f83221d4bc9e5350a8ec0
Raja Ahsaan <utac476@gmail.com>
2026-01-31 21:51:38 +0000

    i  can login right know

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -6,7 +6,7 @@ import { firebaseConfig } from './config';
 
 type FirebaseServices = {
   app: FirebaseApp;
-  auth: Auth | null;
+  auth: Auth;
   firestore: Firestore;
 };
 
@@ -19,9 +19,9 @@ export const initializeFirebase = (): FirebaseServices => {
 
   const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
   const firestore = getFirestore(app);
+  const auth = getAuth(app);
 
-  // Authentication is disabled for public dashboard view
-  firebaseServices = { app, auth: null, firestore };
+  firebaseServices = { app, auth, firestore };
   return firebaseServices;
 };
 
